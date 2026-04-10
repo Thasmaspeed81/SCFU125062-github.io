4cbd63d70703072733ff58835f1ef2c6117f19b7
Thasmaspeed81 <adiyosr7781@gmail.com>
2026-04-10 09:19:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let index = 0;
+let slides = document.getElementsByClassName("slide");
+
+function showSlide(i){
+for(let s of slides){
+    s.classList.remove("active");
+}
+slides[i].classList.add("active");
+}
+
+function nextSlide(){
+index = (index + 1) % slides.length;
+showSlide(index);
+}
+
+function prevSlide(){
+index = (index - 1 + slides.length) % slides.length;
+showSlide(index);
+}
+
 function welcomeMsg(){
 alert("Welcome to the Jurassic Portfolio 🦖🔥");
 }
